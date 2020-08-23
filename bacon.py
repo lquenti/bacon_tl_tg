c6effbf4f1a4784445cc8898e38931f5ee772165
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2020-08-23 19:44:36 +0200

    Implemented messaging queue for handling API rates

--- bacon.py
+++ bacon.py
@@ -3,7 +3,41 @@
 import config as c
 import messages as m
 
-from telegram.ext import Updater, CommandHandler
+import telegram.bot
+from telegram.ext import messagequeue as mq
+from telegram.ext import CommandHandler
+from telegram.utils.request import Request
+from telegram.ext.updater import Updater
+
+
+class MQBot(telegram.bot.Bot):
+    '''
+    A subclass of Bot which delegates send method handling to MQ
+    Extended from: https://github.com/python-telegram-bot/python-telegram-bot/wiki/Avoiding-flood-limits
+    '''
+
+    def __init__(self, *args, is_queued_def=True, mqueue=None, **kwargs):
+        super(MQBot, self).__init__(*args, **kwargs)
+        # below 2 attributes should be provided for decorator usage
+        self._is_messages_queued_default = is_queued_def
+        self._msg_queue = mqueue or mq.MessageQueue()
+
+    def __del__(self):
+        try:
+            self._msg_queue.stop()
+        except:
+            pass
+
+    @mq.queuedmessage
+    def send_message(self, *args, **kwargs):
+        '''Wrapped method would accept new `queued` and `isgroup`
+        OPTIONAL arguments'''
+        return super(MQBot, self).send_message(*args, **kwargs)
+
+    @mq.queuedmessage
+    def send_sticker(self, *args, **kwargs):
+        '''Again, just needed for the decorator'''
+        return super(MQBot, self).send_sticker(*args, **kwargs)
 
 
 def start_handler(update, context):
@@ -31,7 +65,12 @@ def bacon_handler(update, context):
 
 
 def main():
-    updater = Updater(token=c.TOKEN, use_context=True)
+    # The limit is 30/s, but we'll keep it safe because we don't have that much traffic anyways
+    q = mq.MessageQueue(all_burst_limit=25, all_time_limit_ms=1017)
+
+    request = Request(con_pool_size=8)
+    testbot = MQBot(c.TOKEN, request=request, mqueue=q)
+    updater = Updater(bot=testbot, use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start_handler))
